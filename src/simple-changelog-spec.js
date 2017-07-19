14c01e947b1e2c87f2b6404c49c5f4904aa1d704
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-18 23:49:33 -0400

    initial

---
+++ src/simple-changelog-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const simpleChangelog = require('.')
+
+describe('simple-changelog', () => {
+  it('write this test', () => {
+    console.assert(simpleChangelog, 'should export something')
+  })
+})
